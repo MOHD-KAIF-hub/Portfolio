0e5eaa3f4a1202466aae1c89e74232257c6897f3
MOHD-KAIF-hub <mohdkaif30062002@gmail.com>
2023-11-13 11:20:22 +0530

    My commit

--- my-dashboard/src/components/Navbar.jsx
+++ my-dashboard/src/components/Navbar.jsx
@@ -94,40 +94,41 @@ const Navbar = () => {
 
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/mohd-kaif-9381b7228/' target='_blank'
-            >
-              Linkedin <FaLinkedin size={30}  />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://github.com/MOHD-KAIF-hub' target='_blank'
-            >
-              Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://mail.google.com/'  target='_blank'
-            >
-              Email <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1C9MNpYK9AuXzFHVqJIu2fnkWlZ_LyACS/view?usp=drive_link'  target='_blank'
-            >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
-        </ul>
+      <ul>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+    <a
+      className='flex justify-between items-center w-full text-gray-300'
+      href='https://www.linkedin.com/in/mohd-kaif-9381b7228/' target='_blank' rel='noopener noreferrer'
+    >
+      Linkedin <FaLinkedin size={30} />
+    </a>
+  </li>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+    <a
+      className='flex justify-between items-center w-full text-gray-300'
+      href='https://github.com/MOHD-KAIF-hub' target='_blank' rel='noopener noreferrer'
+    >
+      Github <FaGithub size={30} />
+    </a>
+  </li>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+    <a
+      className='flex justify-between items-center w-full text-gray-300'
+      href='https://mail.google.com/' target='_blank' rel='noopener noreferrer'
+    >
+      Email <HiOutlineMail size={30} />
+    </a>
+  </li>
+  <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+    <a
+      className='flex justify-between items-center w-full text-gray-300'
+      href='https://drive.google.com/file/d/1C9MNpYK9AuXzFHVqJIu2fnkWlZ_LyACS/view?usp=drive_link' target='_blank' rel='noopener noreferrer'
+    >
+      Resume <BsFillPersonLinesFill size={30} />
+    </a>
+  </li>
+</ul>
+
       </div>
     </div>
   );
